9ad3b00d9b1b9e748230f192f8b1c43cfe1bfe3a
Sam Nyashev <12237569+Heyter@users.noreply.github.com>
2020-04-15 16:15:04 +0300

    concommand upd

--- includes/extensions/concommand.lua
+++ includes/extensions/concommand.lua
@@ -5,14 +5,7 @@ end
 function concommand.Add(name, callback, helpText, flags)
 	if (not isfunction(callback)) then return end
 	
-	Convars:RegisterCommand(name, function(...)
-		local array = {...}
-		local old_cmd = array[1]
-		
-		table.remove(array, 1)
-
-		callback(Convars:GetCommandClient(), old_cmd, array)
-		old_cmd, array = nil, nil
-		
+	Convars:RegisterCommand(name, function(cmd, ...)
+		callback(Convars:GetCommandClient(), cmd, {...})
 	end, helpText, flags or 0)
 end
